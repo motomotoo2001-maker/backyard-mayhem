9fda22e7245034358bed0b952015c8a43642fd69
motomotoo2001-maker <motomotoo2001@gmail.com>
2026-09-24 18:51:39 +0300

    vfx: enrich player muzzle flash and air blast layers

--- scripts/player/player_vfx_emitter.gd
+++ scripts/player/player_vfx_emitter.gd
@@ -3,8 +3,10 @@ extends Node2D
 
 const MUZZLE_OUTER_COLOR := Color(1.0, 0.56, 0.12, 0.92)
 const MUZZLE_CORE_COLOR := Color(1.0, 0.94, 0.62, 1.0)
+const MUZZLE_SPARK_COLOR := Color(1.0, 0.76, 0.24, 0.82)
 const AIR_INNER_COLOR := Color(0.82, 0.95, 1.0, 0.72)
 const AIR_OUTER_COLOR := Color(0.52, 0.86, 1.0, 0.42)
+const AIR_WISP_COLOR := Color(0.72, 0.92, 1.0, 0.5)
 
 static func build_effect_node(effect_name: StringName, direction: Vector2, effect_scale: float = 1.0) -> Node2D:
     var normalized_direction := direction.normalized() if direction.length_squared() > 0.001 else Vector2.RIGHT
@@ -76,6 +78,32 @@ static func _build_muzzle_flash(root: Node2D) -> void:
     core.color = MUZZLE_CORE_COLOR
     root.add_child(core)
 
+    var spark_top := Line2D.new()
+    spark_top.name = "SparkTop"
+    spark_top.points = PackedVector2Array([
+        Vector2(9, -4),
+        Vector2(28, -15),
+        Vector2(51, -21),
+    ])
+    spark_top.width = 2.4
+    spark_top.default_color = MUZZLE_SPARK_COLOR
+    spark_top.begin_cap_mode = Line2D.LINE_CAP_ROUND
+    spark_top.end_cap_mode = Line2D.LINE_CAP_ROUND
+    root.add_child(spark_top)
+
+    var spark_bottom := Line2D.new()
+    spark_bottom.name = "SparkBottom"
+    spark_bottom.points = PackedVector2Array([
+        Vector2(9, 4),
+        Vector2(28, 15),
+        Vector2(51, 21),
+    ])
+    spark_bottom.width = 2.4
+    spark_bottom.default_color = MUZZLE_SPARK_COLOR
+    spark_bottom.begin_cap_mode = Line2D.LINE_CAP_ROUND
+    spark_bottom.end_cap_mode = Line2D.LINE_CAP_ROUND
+    root.add_child(spark_bottom)
+
 static func _build_air_blast(root: Node2D) -> void:
     var inner := Line2D.new()
     inner.name = "InnerArc"
@@ -106,3 +134,31 @@ static func _build_air_blast(root: Node2D) -> void:
     outer.begin_cap_mode = Line2D.LINE_CAP_ROUND
     outer.end_cap_mode = Line2D.LINE_CAP_ROUND
     root.add_child(outer)
+
+    var upper_wisp := Line2D.new()
+    upper_wisp.name = "UpperWisp"
+    upper_wisp.points = PackedVector2Array([
+        Vector2(10, -8),
+        Vector2(28, -24),
+        Vector2(52, -31),
+        Vector2(78, -24),
+    ])
+    upper_wisp.width = 1.8
+    upper_wisp.default_color = AIR_WISP_COLOR
+    upper_wisp.begin_cap_mode = Line2D.LINE_CAP_ROUND
+    upper_wisp.end_cap_mode = Line2D.LINE_CAP_ROUND
+    root.add_child(upper_wisp)
+
+    var lower_wisp := Line2D.new()
+    lower_wisp.name = "LowerWisp"
+    lower_wisp.points = PackedVector2Array([
+        Vector2(10, 8),
+        Vector2(28, 24),
+        Vector2(52, 31),
+        Vector2(78, 24),
+    ])
+    lower_wisp.width = 1.8
+    lower_wisp.default_color = AIR_WISP_COLOR
+    lower_wisp.begin_cap_mode = Line2D.LINE_CAP_ROUND
+    lower_wisp.end_cap_mode = Line2D.LINE_CAP_ROUND
+    root.add_child(lower_wisp)
